2e1cf824f2f77f7f25f80fc66abcdb618bea5b5f
Andres <andres.malatesta@gmail.com>
2020-06-15 23:28:00 -0300

    Agregado de navegador finalizado

--- notes-app-mongodb/src/server.js
+++ notes-app-mongodb/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path'); // llamo a path para utilizarlo mas abajo en settings
 
 // Initializations
@@ -8,6 +9,16 @@ const app = express();
 app.set('port', process.env.PORT||4000); //para que tome un puerto del servidor y si no da tome el 4000
 app.set('views', path.join(__dirname,'views')); // se indica en donde esta la carpeta view, __dirname va a dar la carpeta en el servidor que puede ser variable y con path se puede concatener carpetas porque puede cabmbiar de acuerdo al SO
 
+//Defino el engine que voy a utilizar para las plantillas
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+// Luego de definir la declaro para utilizarla
+app.set('view engine','.hbs');
+
 // Middleware // funciones que se ejecutan a medida que llegan peticiones
 app.use(express.urlencoded({extended:false})); // Le dice al servidor que cuando llegen los datos de un formulario que se traten de convertir en un objeto json para manipularlo en codigo
 
@@ -15,10 +26,12 @@ app.use(express.urlencoded({extended:false})); // Le dice al servidor que cuando
 
 // Routes // Rutas
 app.get('/',(req,res)=>{
-    res.send('Hello world'); // Defino una ruta basica, nueva ruta nueva url para cuando soliciten una ruta inicial
+    //res.send('Hello world'); // Defino una ruta basica, nueva ruta nueva url para cuando soliciten una ruta inicial
+    res.render('index'); //Ya sabe a donde ir a buscar index porque se declaro views y la extension hbs
 });
 
 // Static files
-app.set(express.static(path.join(__dirname,'public'))); // Se determina en node la carpeta public
+//app.set(express.static(path.join(__dirname,'public'))); // Se determina en node la carpeta public
+app.use(express.static(path.join(__dirname,'public'))); // cambie a use porque estaba solo definida
 
 module.exports = app;
